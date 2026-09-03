5b7b26b449c3a1139fb6dc85cea486e6e4faf2c7
Dayvis <amdayvis39.com>
2026-09-02 22:46:28 -0500

    feat(csharp): add constantes examples

---
+++ 3. Constantes.cs
@@ -0,0 +1,8 @@
+/*Las constantes son variables inmutables que se conocen conocen en tiempo
+de compilación y no cambian durante la vida del programa. Las constantes
+se declaran con el modificador const.*/
+
+const int Months = 12;
+
+// Se puede declarar varios constantes del mismo tipo al mismo tiempo
+const int Weeks = 52, Days = 365;
