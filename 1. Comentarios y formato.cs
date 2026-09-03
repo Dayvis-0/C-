f946a22110cb85fc2e4bbc2c00de3b693750820e
Dayvis <amdayvis39.com>
2026-09-02 22:45:56 -0500

    feat(csharp): add comentarios y formato examples

---
+++ 1. Comentarios y formato.cs
@@ -0,0 +1,12 @@
+using System;
+
+Console.Write("AAA");
+Console.WriteLine("Hola mundo");
+
+// Comentario de una linea
+/*
+Comentario de varias lineas
+*/
+
+// Obtiene el siguiente carácter o tecla de función presionado por el usuario.
+Console.ReadKey();
